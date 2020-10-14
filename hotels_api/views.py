08eb1725d5eee778005c4bb1b292c7604ce71424
ahmad-swedani <swedani390@gmail.com>
2020-10-14 07:24:36 +0300

    add tow classes to views for show all room and show room in details

--- hotels_api/views.py
+++ hotels_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
 
 # Create your views here.
+from .models import Room
+from .serializer import RoomSerializer
+
+class allRooms(generics.ListCreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+class roomDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
