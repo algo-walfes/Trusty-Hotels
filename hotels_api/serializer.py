c8e5a0872b9eec0303f2d90159e3ed81fc6d6ea9
ahmad-swedani <swedani390@gmail.com>
2020-10-18 05:28:39 +0300

    test the app

--- hotels_api/serializer.py
+++ hotels_api/serializer.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 from .models import Room, PostImages
-
+from django.contrib.auth.models import User
+# from django.contrib.auth import authenticate
 
 class ImagesSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,11 +10,10 @@ class ImagesSerializer(serializers.ModelSerializer):
         fields = '__all__'
         # depth = 1
         
-
-
 class RoomSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Room
         fields = ['hotel_name','name','slug','type','price','size','capacity','pets','breakfast','featured','description','extras','published_at', 'tracks']
         depth = 1
+
